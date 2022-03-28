8c68e475c78c56015bdc38b6a908fceaf8bcb442
ritesh151990 <ritesh151990@gmail.com>
2022-03-28 11:48:26 +0530

    get request in homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,41 +1,71 @@
-import React from "react";
+import React,{useState} from "react";
 import "./HomePage.css";
+import MoviesList from "./movies/MoviesList"
 const HomePage = () => {
+  const[movies,setMovies]=useState([]);
+  const fetchMovieshandler =()=> {
+    fetch('https://swapi.dev/api/films/').then(response =>{
+     return response.json();
+    }).then(data =>{
+      const transformedMovies=data.results.map(moviedata =>{
+        return{
+          id :moviedata.episode_id,
+          title: moviedata.title,
+          openingText: moviedata.opening_crawl,
+          releaseDate:moviedata.release_Date
+        }
+      });
+      setMovies(transformedMovies);
+    });
+  
+  }
   return (
-    <div className="homebody">
-      <h3 className="hometitle">Tours</h3>
-      <ul className="homeList">
-        <li>
-          JUL16 <span>DETROIT DTE ENERGY MUSIC THEATRE</span>
-          <button>BUY TICKETS</button>
-        </li>
+        <React.Fragment>
+        <section>
+          <button onClick={fetchMovieshandler}>Fetch Movies</button>
+        </section>
+            <section>
+              <MoviesList movies={movies} />
+            </section>
+        </React.Fragment>
 
-        <li>
-          JUL19 <span>TORONTO,ON BUDWESTER STAGE</span>
-          <button>BUY TICKETS</button>
-        </li>
 
-        <li>
-          JUL22 <span>BRISTOW, VA JIGGY LUBE LIVE</span>
-          <button>BUY TICKETS</button>
-        </li>
 
-        <li>
-          JUL29 <span>PHOENIX, AZ AK-CHIN PAVILION</span>
-          <button>BUY TICKETS</button>
-        </li>
 
-        <li>
-          AUG 2 <span>LAS VEGAS, NV T-MOBILE ARENA</span>
-          <button>BUY TICKETS</button>
-        </li>
+    // <div className="homebody">
+    //   <h3 className="hometitle">Tours</h3>
+    //   <ul className="homeList">
+    //     <li>
+    //       JUL16 <span>DETROIT DTE ENERGY MUSIC THEATRE</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
 
-        <li>
-          AUG 7 <span>CONCORD, CA CONCORD PAVILION</span>
-          <button>BUY TICKETS</button>
-        </li>
-      </ul>
-    </div>
+    //     <li>
+    //       JUL19 <span>TORONTO,ON BUDWESTER STAGE</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
+
+    //     <li>
+    //       JUL22 <span>BRISTOW, VA JIGGY LUBE LIVE</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
+
+    //     <li>
+    //       JUL29 <span>PHOENIX, AZ AK-CHIN PAVILION</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
+
+    //     <li>
+    //       AUG 2 <span>LAS VEGAS, NV T-MOBILE ARENA</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
+
+    //     <li>
+    //       AUG 7 <span>CONCORD, CA CONCORD PAVILION</span>
+    //       <button>BUY TICKETS</button>
+    //     </li>
+    //   </ul>
+    // </div>
   );
 };
 export default HomePage;
